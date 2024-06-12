14b6e2ab1977d07f1f4246c350cad891201a46ac
kmaadcode <90252419+kmaadcode@users.noreply.github.com>
2024-06-12 12:24:33 +0200

    Update utils.js

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -5,7 +5,7 @@ const connectToDB = async () => {
   // const url = process.env.MONGO_URI;
   try {
     if (connection.isConnected) return;
-    const db = await mongoose.connect(process.env.MONGO);    
+    const db = await mongoose.connect(process.env.MONGO_URI);    
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     throw new Error(error);
