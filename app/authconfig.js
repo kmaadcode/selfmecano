6f21881228b5f21c8c9fbe8ab86c7c127168b883
kmaadcode <90252419+kmaadcode@users.noreply.github.com>
2024-06-12 12:21:35 +0200

    Update authconfig.js

--- app/authconfig.js
+++ app/authconfig.js
@@ -6,8 +6,7 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request}) {
-      const isLoggedIn = auth?.user;
-      console.log(isLoggedIn + " !!!!!! in authConfig !!!!!");
+      const isLoggedIn = auth?.user;     
       const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
